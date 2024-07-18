c4687fae8bd9b4025e82d3f1b6ba181270fdbc98
Iago Munoz <iago.rmunoz@gmail.com>
2024-07-18 17:24:54 -0300

    Updates

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
 
   // Precache files
-  workbox.precaching.precacheAndRoute([{"revision":"a4106fe0eb83b646c4115067b6613968","url":"index.html"},{"revision":"524aa766cd63f7b0895d77f809fc0770","url":"static/css/main.692caf56.css"},{"revision":"1eda53d6c5f2119464b255eab2af7d1c","url":"static/js/453.ed3810f9.chunk.js"},{"revision":"3ecbd9668ef7f73cfede8d7b6a757b02","url":"static/js/main.f050a521.js"}]);
+  workbox.precaching.precacheAndRoute([{"revision":"e7442bc46ad0a09005179e39d25ce489","url":"index.html"},{"revision":"ab03f821616703b81001cfd3f9b510bd","url":"static/css/main.1d05073d.css"},{"revision":"1eda53d6c5f2119464b255eab2af7d1c","url":"static/js/453.ed3810f9.chunk.js"},{"revision":"eb431811f1f6014da25d096ab841c4e4","url":"static/js/main.4c756831.js"}]);
 
   // Cache JavaScript files
   workbox.routing.registerRoute(
