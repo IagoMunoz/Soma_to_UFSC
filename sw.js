9e9ce27a7e82a3c4eb6db8dc6548bb0042c3995b
Iago Munoz <iago.rmunoz@gmail.com>
2024-07-19 14:33:12 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,23 +1,10 @@
-// src/sw.js
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
-
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-
-  // Precache files
-  workbox.precaching.precacheAndRoute([{"revision":"a40e629ee2a2d9a6c160d74736b907bd","url":"index.html"},{"revision":"9d05146942ed0d9b4a17c4377ac0b696","url":"static/css/main.c3c223a5.css"},{"revision":"8d866372437cb21332ac2eada41309e1","url":"static/js/main.c5ab23ef.js"}]);
-
-  // Cache JavaScript files
-  workbox.routing.registerRoute(
-    new RegExp('\\.js$'),
-    new workbox.strategies.NetworkFirst()
-  );
-
-  // Cache CSS files
-  workbox.routing.registerRoute(
-    /.*\.css$/,
-    new workbox.strategies.StaleWhileRevalidate({
-      cacheName: 'css-cache',
-    })
-  );
-}
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        console.log('Service Worker registrado com sucesso com escopo:', registration.scope);
+      }, function(err) {
+        console.log('Registro de Service Worker falhou:', err);
+      });
+    });
+  }
+  
