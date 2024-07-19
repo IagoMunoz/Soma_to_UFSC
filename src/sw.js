c877e71ab427ebde079361fd4c54ea976d197a06
Iago Munoz <iago.rmunoz@gmail.com>
2024-07-19 01:56:16 -0300

    a

--- src/sw.js
+++ src/sw.js
@@ -3,61 +3,26 @@
 // Import the Workbox libraries
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
+// Apply caching strategies
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'document',
+  new workbox.strategies.NetworkFirst()
+);
 
-  // Precache files
-  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+// Force immediate updates for all resources, ensuring users get the latest version
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Forces the waiting service worker to become the active service worker
+});
 
-  // Cache strategies for various types of requests
-  workbox.routing.registerRoute(
-    new RegExp('\\.js$'),
-    new workbox.strategies.NetworkFirst()
-  );
-
-  workbox.routing.registerRoute(
-    /.*\.css$/,
-    new workbox.strategies.StaleWhileRevalidate({
-      cacheName: 'css-cache',
-    })
-  );
-
-  workbox.routing.registerRoute(
-    /.*\.(?:png|jpg|jpeg|svg|gif)$/,
-    new workbox.strategies.CacheFirst({
-      cacheName: 'image-cache',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxAgeSeconds: 7 * 24 * 60 * 60, // Cache for one week
-          maxEntries: 20, // Only cache 20 images
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim()); // Allows the service worker to start controlling open clients
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          return caches.delete(cacheName); // Clears old caches
         })
-      ],
+      );
     })
   );
-
-  workbox.routing.registerRoute(
-    ({request}) => request.destination === 'document',
-    new workbox.strategies.NetworkFirst()
-  );
-
-  // Force immediate updates for all resources, ensuring users get the latest version
-  self.addEventListener('install', (event) => {
-    self.skipWaiting(); // Forces the waiting service worker to become the active service worker
-  });
-
-  self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim()); // Allows the service worker to start controlling open clients
-    event.waitUntil(
-      caches.keys().then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            return caches.delete(cacheName); // Clears old caches
-          })
-        );
-      })
-    );
-  });
-  
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+});
