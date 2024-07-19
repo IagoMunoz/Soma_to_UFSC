f76da4d9cd4a70278c910d13b5bf0fa93bc11da3
Iago Munoz <iago.rmunoz@gmail.com>
2024-07-19 02:14:07 -0300

    retornando

--- src/index.js
+++ src/index.js
@@ -2,19 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker'; // Corrigido o caminho
-
-function isMobileDevice() {
-  // Detecta dispositivos móveis pelo userAgent
-  return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-}
-
-function showUpdateButton() {
-  if (isMobileDevice()) {
-    const updateButton = document.getElementById('updateButton');
-    updateButton.style.display = 'block';
-  }
-}
+import { registerServiceWorker } from './serviceWorker'
+registerServiceWorker();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -22,15 +11,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Register the service worker
-serviceWorker.register({
-  onUpdate: registration => {
-    if (registration && registration.waiting) {
-      showUpdateButton();
-    }
-  }
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
