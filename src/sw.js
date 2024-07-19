b7fbf257fc5a1ddc8048c7a46a31d441d6df738a
Iago Munoz <iago.rmunoz@gmail.com>
2024-07-19 01:15:55 -0300

    desistindo do botao e tentando contornar com event listener ao abrir o app

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,6 @@
 // src/sw.js
+
+// Import the Workbox libraries
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
 if (workbox) {
@@ -7,13 +9,12 @@ if (workbox) {
   // Precache files
   workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-  // Cache JavaScript files
+  // Cache strategies for various types of requests
   workbox.routing.registerRoute(
     new RegExp('\\.js$'),
     new workbox.strategies.NetworkFirst()
   );
 
-  // Cache CSS files
   workbox.routing.registerRoute(
     /.*\.css$/,
     new workbox.strategies.StaleWhileRevalidate({
@@ -21,7 +22,6 @@ if (workbox) {
     })
   );
 
-  // Cache image files
   workbox.routing.registerRoute(
     /.*\.(?:png|jpg|jpeg|svg|gif)$/,
     new workbox.strategies.CacheFirst({
@@ -34,6 +34,30 @@ if (workbox) {
       ],
     })
   );
+
+  workbox.routing.registerRoute(
+    ({request}) => request.destination === 'document',
+    new workbox.strategies.NetworkFirst()
+  );
+
+  // Force immediate updates for all resources, ensuring users get the latest version
+  self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Forces the waiting service worker to become the active service worker
+  });
+
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim()); // Allows the service worker to start controlling open clients
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            return caches.delete(cacheName); // Clears old caches
+          })
+        );
+      })
+    );
+  });
+  
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
