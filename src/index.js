58e39ccd8ff3c5a76e521db1e3a9dde18c17369d
Iago Munoz <iago.rmunoz@gmail.com>
2024-07-19 19:13:54 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -8,33 +8,14 @@ import { registerServiceWorker, unregister } from './serviceWorker.js';
 unregister();
 registerServiceWorker();
 
-document.addEventListener('visibilitychange', () => {
-    if (document.visibilityState === 'visible') {
-        // Função que você deseja rodar quando o app for reaberto
-        console.log('App reaberto - rodando função de atualização');
-        atualizarApp();
-    }
-});
-
-function atualizarApp() {
-    // Código para atualizar o aplicativo, como fazer uma nova requisição de dados, etc.
-    fetch('/path/to/your/api/endpoint')
-        .then(response => response.json())
-        .then(data => {
-            // Atualize sua aplicação com os novos dados
-            console.log('Dados atualizados:', data);
-        })
-        .catch(error => console.error('Erro ao atualizar:', error));
-}
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+  </React.StrictMode>
 );
 
-// Se você estiver usando o serviceWorker para permitir o PWA, deixe-o registrado
-registerServiceWorker();
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
