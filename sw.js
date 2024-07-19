3868b6c5a81f91608ea15b02cbe2036103702e2d
Iago Munoz <iago.rmunoz@gmail.com>
2024-07-19 19:38:58 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
+// src/sw.js
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
 
 if (workbox) {
-  console.log("Yay! Workbox is loaded 🎉");
+  console.log(`Yay! Workbox is loaded 🎉`);
 
   // Precache files
   workbox.precaching.precacheAndRoute([{"revision":"298b518636b8933d347ca8ff1ef9e6d1","url":"index.html"},{"revision":"0e1c5c4bf4b7eaaa90e2a80ba7f85994","url":"static/css/main.d3229d4f.css"},{"revision":"d0f139f203b4f12097a24e54698b936e","url":"static/js/453.620ef83d.chunk.js"},{"revision":"7c02a7c0beeed4fb9d921e45db470949","url":"static/js/main.d5b6e185.js"}]);
 
   // Cache JavaScript files
   workbox.routing.registerRoute(
-    /\.js$/,
+    new RegExp('\\.js$'),
     new workbox.strategies.NetworkFirst()
   );
 
   // Cache CSS files
   workbox.routing.registerRoute(
-    /\.css$/,
+    /.*\.css$/,
     new workbox.strategies.StaleWhileRevalidate({
       cacheName: 'css-cache',
     })
@@ -22,7 +23,7 @@ if (workbox) {
 
   // Cache image files
   workbox.routing.registerRoute(
-    /\.(?:png|jpg|jpeg|svg|gif)$/,
+    /.*\.(?:png|jpg|jpeg|svg|gif)$/,
     new workbox.strategies.CacheFirst({
       cacheName: 'image-cache',
       plugins: [
@@ -34,5 +35,5 @@ if (workbox) {
     })
   );
 } else {
-  console.log("Boo! Workbox didn't load 😬");
+  console.log(`Boo! Workbox didn't load 😬`);
 }
