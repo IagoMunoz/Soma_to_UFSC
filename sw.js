9812da298e9b02181f9e548e7db49c120304e8d6
Iago Munoz <iago.rmunoz@gmail.com>
2024-07-19 19:06:01 -0300

    Updates

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
 
   // Precache files
-  workbox.precaching.precacheAndRoute([{"revision":"298b518636b8933d347ca8ff1ef9e6d1","url":"index.html"},{"revision":"0e1c5c4bf4b7eaaa90e2a80ba7f85994","url":"static/css/main.d3229d4f.css"},{"revision":"d0f139f203b4f12097a24e54698b936e","url":"static/js/453.620ef83d.chunk.js"},{"revision":"7c02a7c0beeed4fb9d921e45db470949","url":"static/js/main.d5b6e185.js"}]);
+  workbox.precaching.precacheAndRoute([{"revision":"5858778213463a7beb2c5c8818cd1cf4","url":"index.html"},{"revision":"0e1c5c4bf4b7eaaa90e2a80ba7f85994","url":"static/css/main.d3229d4f.css"},{"revision":"d0f139f203b4f12097a24e54698b936e","url":"static/js/453.620ef83d.chunk.js"},{"revision":"a9d84ddf55ec5f90a50d91e51207682c","url":"static/js/main.85891bdb.js"}]);
 
   // Cache JavaScript files
   workbox.routing.registerRoute(
@@ -34,6 +34,38 @@ if (workbox) {
       ],
     })
   );
+
+  // Event listeners for Service Worker lifecycle
+  self.addEventListener('install', event => {
+    console.log('Service Worker instalado');
+    self.skipWaiting();
+  });
+
+  self.addEventListener('activate', event => {
+    console.log('Service Worker ativado');
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cache => {
+            if (cache !== 'your-cache-name') {
+              console.log('Service Worker limpando caches antigos');
+              return caches.delete(cache);
+            }
+          })
+        );
+      })
+    );
+  });
+
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          return response || fetch(event.request);
+        })
+    );
+  });
+
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
